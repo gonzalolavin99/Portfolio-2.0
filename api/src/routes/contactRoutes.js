04e5a4f357bb9e9b69c98ea2b39333bc7c729dcf
Gonzalo Lavin <gonzalolavin99@gmail.com>
2024-09-02 21:42:22 -0400

    UPDATE: Changes in the router file to use the validate contact middleware

--- api/src/routes/contactRoutes.js
+++ api/src/routes/contactRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contactController');
+const validateContact = require('../middlewares/validateContact');
 
-router.post('/submit', contactController.submitContact);
+router.post('/submit', validateContact, contactController.submitContact);
 
 module.exports = router;
