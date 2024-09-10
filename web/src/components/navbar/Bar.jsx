09e7c4238a5302125135d7aa09b1e73134086e79
Gonzalo Lavin <gonzalolavin99@gmail.com>
2024-09-10 01:40:28 -0300

    UPDATE: Responsive navbar change

--- web/src/components/navbar/Bar.jsx
+++ web/src/components/navbar/Bar.jsx
@@ -5,7 +5,13 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import CasinoIcon from '@mui/icons-material/Casino';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 const pages = [
   { name: 'Projects', id: 'projects' },
@@ -14,50 +20,97 @@ const pages = [
 ];
 
 const Bar = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   const handleScrollTo = (id) => {
     const element = document.getElementById(id);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
+    handleCloseNavMenu();
   };
 
   return (
     <AppBar position="static" sx={{bgcolor:'rgb(189, 189, 189)'}} className='appbar'>
       <Toolbar>
-        <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
-          <CasinoIcon sx={{ mr: 1, color: 'black' }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#"
-            sx={{
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'black',
-              textDecoration: 'none',
-            }}
-          >
-            Gonzalo Lavín
-          </Typography>
-        </Box>
-        <Box sx={{ display: 'flex' }}>
-          {pages.map((page) => (
-            <Button
-              key={page.name}
-              onClick={() => handleScrollTo(page.id)}
-              sx={{ 
-                color: 'black', 
-                '&:hover': {
-                  backgroundColor: "#5C5C5C"
-                } 
-              }}
+        <CasinoIcon sx={{ mr: 1, color: 'black' }} />
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="#"
+          sx={{
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'black',
+            textDecoration: 'none',
+            flexGrow: 1,
+          }}
+        >
+          Gonzalo Lavín
+        </Typography>
+        {isMobile ? (
+          <Box>
+            <IconButton
+              size="large"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
             >
-              {page.name}
-            </Button>
-          ))}
-        </Box>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.name} onClick={() => handleScrollTo(page.id)}>
+                  <Typography textAlign="center">{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        ) : (
+          <Box sx={{ display: 'flex' }}>
+            {pages.map((page) => (
+              <Button
+                key={page.name}
+                onClick={() => handleScrollTo(page.id)}
+                sx={{ 
+                  color: 'black', 
+                  '&:hover': {
+                    backgroundColor: "#5C5C5C"
+                  } 
+                }}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
+        )}
       </Toolbar>
     </AppBar>
   )
