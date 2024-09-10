db355e23f92029a9c18e3a264be68b5815586b27
Gonzalo Lavin <gonzalolavin99@gmail.com>
2024-09-09 23:41:16 -0300

    UPDATE: url front

--- api/src/app.js
+++ api/src/app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const contactRoutes = require('./routes/contactRoutes');
-const sequelize = require('./config/db.js');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
 
-const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
+const allowedOrigins = ["http://localhost:5173", "https://gonzalo-lavin.vercel.app"];
 // Configuración de CORS
 const corsOptions = {
   origin: (origin, callback) => {
-    console.log("Se esta validando el siguiente origin: "+origin)
+    console.log("Se esta validando el siguiente origin: "  +origin)
     if (allowedOrigins.find((o) => o == origin) || env().isLocal) {
     
       callback(null, true);
